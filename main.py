22f8e1c41471be203b56abe6de237bd266055c3a
Abu Faiz <61959407+faiz-2614@users.noreply.github.com>
2021-08-05 01:33:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ response.raise_for_status()
 
 weather_data = response.json()
 
-account_sid = "AC0a45514966c5c318b4e2bc4203d2ba40"
-auth_token = "1aa7b6f6dffb19524046fb14c4d23ce7"
+account_sid = "Your twillio accnt_sid"
+auth_token = "acoount_token"
 
 will_rain = False
 
@@ -25,7 +25,7 @@ if will_rain:
     message = client.messages \
         .create(
         body="Well,It's About To Rain",
-        from_='+16238007606',
-        to ='+916370196987'
+        from_='+twillio num',
+        to =''your_num
     )
     print(message.status)
